d9eebc8a2f6dc9b0692750af3a65a1114ef594a1
zgumby85 <zhamby1@ivytech.edu>
2022-05-04 09:46:40 -0400

    front-end-with-auth

---
+++ init.js
@@ -0,0 +1,6 @@
+const auth = new Auth()
+
+document.querySelector("#logout").addEventListener("click", (e)=> {
+    auth.logOut()
+})
+
---
+++ js/auth.js
@@ -0,0 +1,25 @@
+class Auth{
+    constructor(){
+        document.querySelector("body").style.display = "none"
+        const auth = localStorage.getItem("auth")
+        this.validateAuth(auth)
+    }
+
+    validateAuth(auth){
+        if(auth != 1){
+            window.location.replace("/login.html")
+        }
+        else{
+            document.querySelector("body").style.display = "block"
+        }
+    }
+
+    logOut(){
+        localStorage.removeItem("auth")
+        localStorage.removeItem("token")
+        localStorage.removeItem("uname")
+
+        window.location.replace("/login.html")
+
+    }
+}
